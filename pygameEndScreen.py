a5214ef34081e3d9700e5fee78197a43d26d6532
Jared Denning <den17022@byui.edu>
2021-03-23 15:52:32 -0600

    jareds changes

--- pygameEndScreen.py
+++ pygameEndScreen.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame.freetype
 import pygame_gui
 
 def rotate(surface, angle):
@@ -7,12 +8,33 @@ def rotate(surface, angle):
     return rotated_surface, rotated_rect
 
 # Phrase of Matching
-def youMatch(screen):
+def youMatch(screen, final_time):
+
+    #font needed to render the highscore text
+    font = pygame.freetype.SysFont('microsoftjhengheimicrosoftjhengheiuibold', 30)
+    #creating the reading/writing object
+    
+
+    
+#%%
+
+    highscore = open(r'highscores.txt', 'r')
+    print(highscore.read())
+    highscore.close()
+    highscore = open(r'highscores.txt', 'a')
+    highscore.write(str(final_time) + '\n')
+    highscore.close()
+    highscore = open(r'highscores.txt', 'r')
+    print(highscore.read())
+    highscore.close()
+
+#%%
+
     clock = pygame.time.Clock()
     running = True
     size = 100
     angle = 0
-    phrase = pygame.font.SysFont('Cooper Black', size).render("Good Job!", False, (105,105,105))
+    phrase = pygame.font.SysFont('microsoftjhengheimicrosoftjhengheiuibold', size).render("Good Job!", False, (105,105,105))
     while running:
         clock.tick(40)
         for event in pygame.event.get():
@@ -23,7 +45,9 @@ def youMatch(screen):
         angle += 1
 
         screen.fill((0,0,0))
+        font.render_to(screen, (300, 500), ("Your Time:" + str(int(final_time))),(105,105,105))
         phrase_rotated, phrase_rotated_rect = rotate(phrase, angle)
 
         screen.blit(phrase_rotated, phrase_rotated_rect)
         pygame.display.update() 
+# %%
